626b1dd4cd0aa2c4dba4ceb1720a2b623064cde0
emilycw <emily.rose.cw@gmail.com>
2015-07-02 10:50:47 -0700

    start game skeleton

--- game.rb
+++ game.rb
@@ -1,2 +1,21 @@
 class Game
+	def initialize(player1, player2)
+		@players = [player1, player2]
+		@board = board.new
+		@board.populate_board
+	end
+
+	def play
+		until over?
+			play_turn
+		end
+		puts "Game is over, #{players.last} has won."
+	end
+
+	def over?
+	end
+
+	def play_turn
+	end
+
 end
